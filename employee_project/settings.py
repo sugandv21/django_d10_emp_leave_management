69a0698cf9ce237bad2e2c8c296bc85708efbf78
sugandv21 <sdvsuganya@gmail.com>
2025-09-02 22:55:05 +0530

    Update settings.py

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -115,9 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = "static/"
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 #static storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -128,3 +129,4 @@ LOGOUT_REDIRECT_URL = "home"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
